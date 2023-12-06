b2595fcb6c7244588289adb93625b3a3ae743eee
adntian <china2008tian@126.com>
2023-12-06 22:28:59 +0800

    从文件读取助记词

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const { TonClient, WalletContractV4, internal } = require("@ton/ton");
 const { mnemonicToPrivateKey } = require("@ton/crypto");
 const https = require('https');
 
+const fs = require('fs');
+const path = require('path');
+
 // Create Client
 const client = new TonClient({
-  endpoint:
-    'https://ton.access.orbs.network/55B1c0ff5Bd3F8B62C092Ab4D238bEE463E655B1/1/mainnet/toncenter-api-v2/jsonRPC',
-  //"https://ton.access.orbs.network/44A2c0ff5Bd3F8B62C092Ab4D238bEE463E644A2/1/mainnet/toncenter-api-v2/jsonRPC",
+    endpoint:
+        "https://ton.access.orbs.network/55B1c0ff5Bd3F8B62C092Ab4D238bEE463E655B1/1/mainnet/toncenter-api-v2/jsonRPC",
+        //"https://ton.access.orbs.network/44A2c0ff5Bd3F8B62C092Ab4D238bEE463E644A2/1/mainnet/toncenter-api-v2/jsonRPC",
 });
+
 
 // 最大尝试多少次
 const maxTimes = 10000;
@@ -48,10 +52,37 @@ async function main(mnemonic, index) {
       console.log(transfer);
       count++;
       console.log(`第${index}个钱包，第${count}次成功`);
-    } catch (error) {
+    } catch (error) {console.log(error)}
+  }
+}
+
+const getPhrase = () => {
+  try {
+  const phrases = fs.readFileSync(path.join(__dirname, './phrases.txt'), 'utf-8');
+  return phrases.split('\n');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log('未发现phrases.txt文件，已自动创建文件')
+      fs.writeFileSync(path.join(__dirname, './phrases.txt'), '');
+    } else {
       console.log(error);
     }
+    return [];
   }
+}
+
+const mnemonicList = getPhrase().filter(t=>t && t.indexOf('#')==-1 && (t.split(' ').length === 12 || t.split(' ').length === 24));
+
+if (mnemonicList.length === 0) {
+    console.error(`
+    ******************************************************
+    未发现有效的钱包助记词，请在当前目录的phrases.txt文件中填写
+    需要12位或24位的助记词，每行一个
+    可以添加注释，以#开头即可，以下为文件示例：
+    # 这是注释 我的钱包
+    word1 word2 word3 word4 word5 word6 word7 word8 word9 word10 word11 word12 word13 word14 word15 word16 word17 word18 word19 word20 word21 word22 word23 word24
+    ******************************************************`)
+  return
 }
 
 const checkStatus = (addr) => {
@@ -70,40 +101,23 @@ const checkStatus = (addr) => {
       });
     });
   });
-};
-
-/*
- * 多个钱包助记词分别用英文逗号分隔，例如
- * [
- *   'word1 word2 .... word24',
- *   'word1 word2 .... word24'
- * ]
- */
-let mnemonics = [];
-if (mnemonics.length === 0) {
-    console.log('请填写钱包助记词')
-    return
 }
+
 const run = () => {
   console.log('检测状态');
-  checkStatus('EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c').then(
-    (res) => {
-      if (res) {
-        mnemonics.forEach((t, index) => {
-          main(t, index + 1);
-        });
-      } else {
-        const waitTime = 10;
-        console.log(
-          `合约被冻结，等待${waitTime}秒后重试，当前时间：`,
-          new Date().toLocaleString()
-        );
-        setTimeout(() => {
-          run();
-        }, waitTime * 1000);
-      }
+  checkStatus('EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c').then((res) => {
+    if (res) {
+      mnemonicList.forEach((t, index) => {
+        main(t, index + 1);
+      });
+    } else {
+      const waitTime = 10;
+      console.log(`合约被冻结，等待${waitTime}秒后重试，当前时间：`, new Date().toLocaleString());
+      setTimeout(() => {
+        run();
+      }, waitTime * 1000);
     }
-  );
-};
+  });
+}
 
 run();
