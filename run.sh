a26682e326023fcc57b61029e38eb1106854fc9d
adntian <china2008tian@126.com>
2023-12-06 21:05:10 +0800

    日志输出

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npm install @ton/ton @ton/crypto @ton/core buffer && nohup node index.js 2>&1 & >> mint.log
+npm install @ton/ton @ton/crypto @ton/core buffer && nohup node index.js 2>&1 >> mint.log &
