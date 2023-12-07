97a6ca798c5b6faac3236154bad0b3cb30f76cda
田振洲 <china2008tian@126.com>
2023-12-07 13:57:34 +0800

    feat:注释延时，优化逻辑

--- index.js
+++ index.js
@@ -10,15 +10,9 @@ const path = require('path');
 const maxTimes = 10000;
 
 async function main(mnemonic, index) {
-  const mnemonics = mnemonic.split(' ');
-  let keyPair = await mnemonicToPrivateKey(mnemonics);
-  let workchain = 0;
-  let wallet = WalletContractV4.create({
-    workchain,
-    publicKey: keyPair.publicKey,
-  });
+
 // Create Client
-const client = new TonClient({
+  const client = new TonClient({
     endpoint:
         "https://toncenter.com/api/v2/jsonRPC",
         // "https://ton.access.orbs.network/55B2c0ff5Bd3F8B62C092Ab4D238bEE463E655B2/1/mainnet/toncenter-api-v2/jsonRPC"
@@ -27,12 +21,20 @@ const client = new TonClient({
 });
 
 
+  const mnemonics = mnemonic.split(' ');
+  let keyPair = await mnemonicToPrivateKey(mnemonics);
+  let workchain = 0;
+  let wallet = WalletContractV4.create({
+    workchain,
+    publicKey: keyPair.publicKey,
+  });
+
   try {
-     await sleep(1500);
-  let contract = client.open(wallet);
-  console.log(wallet.address + ' 开始运行');
-  let balance = await contract.getBalance();
-  console.log(`第${index}个钱包：【${wallet.address}  】，余额：${balance}`)
+    // await sleep(1500);
+    let contract = client.open(wallet);
+    console.log(wallet.address + ' 开始运行');
+    let balance = await contract.getBalance();
+    console.log(`第${index}个钱包：【${wallet.address}  】，余额：${balance}`)
     if (balance == 0) {
       console.log(`第${index}个钱包：【${wallet.address}  】，余额为0，3分钟后重试`)
       await sleep(180000);
@@ -40,48 +42,51 @@ const client = new TonClient({
     }
 
 
-  let v = [];
+    let v = [];
 
-  for (let i = 0; i < 4; i++) {
-    v.push(
-      internal({
-        to: `EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c`,
-        value: '0',
-        body: 'data:application/json,{"p":"ton-20","op":"mint","tick":"nano","amt":"100000000000"}'
-      })
-    );
-  }
-  let count = 0;
-  let seqno = -1;
-  let lastSuccess = true
-  let lastError = ''
-
-  for (let i = 0; i < maxTimes; i++) {
-    // await sleep(1000);
-    try {
-      if (seqno === -1 || lastSuccess || lastError.indexOf('seqno') > -1) {
-        seqno = await contract.getSeqno();
-      }
-      console.log('seqno' , seqno);
-      // await sleep(1100);
-      let transfer = await contract.sendTransfer({
-        seqno: seqno,
-        secretKey: keyPair.secretKey,
-        validUntil: Math.floor(Date.now() / 1e3) + 600,
-        messages: v,
-      });
-      console.log(transfer);
-      count++;
-      console.log(`第${index}个钱包：【${wallet.address}  】，第${count}次成功`);
-      lastSuccess = true
-    } catch (error) {
-      lastSuccess = false
-      console.log(`第${index}个钱包：【${wallet.address}  】`, error.response.data.code, error.response.data.error)
+    for (let i = 0; i < 4; i++) {
+      v.push(
+        internal({
+          to: `EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c`,
+          value: '0',
+          body: 'data:application/json,{"p":"ton-20","op":"mint","tick":"nano","amt":"100000000000"}'
+        })
+      );
     }
-    
-  }
+    let count = 0;
+    let seqno = -1;
+    let lastSuccess = true
+    let lastError = ''
+
+    for (let i = 0; i < maxTimes; i++) {
+      // await sleep(1000);
+      try {
+//      if (seqno === -1 || lastSuccess || lastError.indexOf('seqno') > -1) {
+        seqno = await contract.getSeqno();
+//      }
+//       console.log('seqno' , seqno);
+        // await sleep(300);
+        let transfer = await contract.sendTransfer({
+          seqno: seqno,
+          secretKey: keyPair.secretKey,
+          validUntil: Math.floor(Date.now() / 1e3) + 600,
+          messages: v,
+        });
+        count++;
+        console.log(`第${index}个钱包：【${wallet.address}  】，第${count}次成功, seqno: ${seqno}, resp： ${transfer}, 当前时间：`, new Date().toLocaleString());
+        lastSuccess = true
+      } catch (error) {
+        lastSuccess = false
+        console.log(`第${index}个钱包：【${wallet.address}  】78:`, error.response.data.code, error.response.data.error)
+        if (error.response.data.code === undefined) {
+          console.log('error.response.data.code === undefined', error.response.status, error.response.statusText)
+        }
+//      await sleep(100);
+      }
+
+    }
   } catch (err) {
-    console.log('create client error', err.response && err.response.data ? err.response.data.code : err.response, err.response && err.response.data ? err.response.data.error : '')
+    console.log('create client error', err.response && err.response.data ? err.response.data.code : err.response, err.response && err.response.data ? err.response.data.error : '', err)
     console.log(`重试第${index}个钱包`)
     main(mnemonic, index)
   }
@@ -94,8 +99,8 @@ const sleep = (time) => {
 
 const getPhrase = () => {
   try {
-  const phrases = fs.readFileSync(path.join(__dirname, './phrases.txt'), 'utf-8');
-  return phrases.split('\n');
+    const phrases = fs.readFileSync(path.join(__dirname, './phrases.txt'), 'utf-8');
+    return phrases.split('\n');
   } catch (error) {
     if (error.code === 'ENOENT') {
       console.log('未发现phrases.txt文件，已自动创建文件')
@@ -107,10 +112,10 @@ const getPhrase = () => {
   }
 }
 
-const mnemonicList = getPhrase().map(t => t ? t.trim() : '').filter(t=>t && t.indexOf('#')==-1 && (t.split(' ').length === 12 || t.split(' ').length === 24));
+const mnemonicList = getPhrase().map(t => t ? t.trim() : '').filter(t => t && t.indexOf('#') == -1 && (t.split(' ').length === 12 || t.split(' ').length === 24));
 
 if (mnemonicList.length === 0) {
-    console.error(`
+  console.error(`
     ******************************************************
     未发现有效的钱包助记词，请在当前目录的phrases.txt文件中填写
     需要12位或24位的助记词，每行一个
@@ -120,7 +125,7 @@ if (mnemonicList.length === 0) {
     ******************************************************`)
   return
 } else {
-    console.log(`本次共发现${mnemonicList.length}个助记词`)
+  console.log(`本次共发现${mnemonicList.length}个助记词`)
 }
 
 const checkStatus = (addr) => {
@@ -141,9 +146,9 @@ const checkStatus = (addr) => {
   });
 }
 
-        mnemonicList.forEach((t, index) => {
-          main(t, index + 1);
-        });
+mnemonicList.forEach((t, index) => {
+  main(t, index + 1);
+});
 
 /*
 const run = () => {
